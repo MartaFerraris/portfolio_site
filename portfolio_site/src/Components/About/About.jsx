7969cfd9534235f79834226bf36552ccbb863322
Marta <marta.ferraris25@gmail.com>
2024-07-17 15:22:16 +0200

    add About component and his css file, and correct the svg for the index file

---
+++ portfolio_site/src/Components/About/About.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./About.css"
+import theme_pattern from "../../assets/Theme_pattern.png"
+import profile_img from "../../assets/Profile.png"
+
+const About = () => {
+    return (
+        <div className='about'>
+            <div className="about-title">
+                <h1>About me</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="about-sections">
+                <div className="about-left">
+                    <img src={profile_img} alt="" />
+                </div>
+                <div className="about-right">
+                    <div className="about-paragraph">
+                        <p>Appena penso qualcosa lo metto; Appena penso qualcosa lo metto;Appena penso qualcosa lo metto;Appena penso qualcosa lo metto;Appena penso qualcosa lo metto;Appena penso qualcosa lo metto.</p>
+                        <p>Appena penso qualcosa lo metto; Appena penso qualcosa lo metto;Appena penso qualcosa lo metto;Appena penso qualcosa lo metto;Appena penso qualcosa lo metto;Appena penso qualcosa lo metto.</p>
+                    </div>
+                    <div className="about-skills">
+                        <div className="about-single-skill">
+                            <p>HTML & CSS</p><hr style={{width:"50%"}} />
+                        </div>
+                        <div className="about-single-skill">
+                            <p>React JS</p><hr style={{width:"70%"}} />
+                        </div>
+                        <div className="about-single-skill">
+                            <p>Javascript</p><hr style={{width:"60%"}} />
+                        </div>
+                        <div className="about-single-skill">
+                            <p>Next JS</p><hr style={{width:"50%"}} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="about-achievements">
+                <div className="about-achievement">
+                    <h1>1+</h1>
+                    <p>Year of experience</p>
+                </div>
+                <hr />
+                <div className="about-achievement">
+                    <h1>numero di progetti</h1>
+                    <p>progetti completati</p>
+                </div>
+                <hr />
+            </div>
+        </div>
+    )
+}
+
+export default About
