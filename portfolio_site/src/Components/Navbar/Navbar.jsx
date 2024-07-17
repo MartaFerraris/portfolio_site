b6819c7e2fa5ad90dca33f871b62f1441d833c64
Marta <marta.ferraris25@gmail.com>
2024-07-18 00:47:39 +0200

    add a component named Button and implemented in the other files

--- portfolio_site/src/Components/Navbar/Navbar.jsx
+++ portfolio_site/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/Logo.svg'
+import Button from '../Button/Button'
 
 const Navbar = () => {
+
+  const redirect = () => window.open('https://linktr.ee/MartaDeveloper', '_blank')
+
   return (
     <div className='navbar'>
       <img className='nav-logo' src={logo} alt="" />
@@ -13,9 +17,7 @@ const Navbar = () => {
         <li>Portfolio</li>
         <li>Contact</li>
       </ul>
-      <div className="nav-connect" onClick={() => window.open('https://linktr.ee/MartaDeveloper', '_blank')}>
-        My contacts
-      </div>
+      <Button text={"My Contacts"} onClick={redirect}/>
 
     </div>
   )
--- portfolio_site/src/Components/Profile/Profile.jsx
+++ portfolio_site/src/Components/Profile/Profile.jsx
@@ -8,12 +8,12 @@ const Profile = () => {
             <img src={profileImg} alt="profile image" />
             <h1><span>I'm Marta Ferraris</span>, <br />frontend developer based in Italy.</h1>
             <p>I'm a frontend developer from Biella in Piedmont, and this is my website!</p>
-            <div className="profile-action">
+            {/* <div className="profile-action">
                 <div className="profile-connect">
                     Connect with me
                 </div>
                 <div className="profile-resume">My resume</div>
-            </div>
+            </div> */}
         </div>
     )
 }
